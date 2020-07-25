a6a9c06e03ea0b7d1c4eb68d72fe62f236d2689a
zhanghaiyu <zhanghaiyu@cmcm.com>
2020-07-25 16:29:07 +0800

    二叉树最小深度

---
+++ algorithm/leetcode/min_depth.go
@@ -0,0 +1,39 @@
+package leetcode
+
+//111. 二叉树的最小深度
+//给定一个二叉树，找出其最小深度。
+//
+//最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+//
+//说明: 叶子节点是指没有子节点的节点。
+//
+//示例:
+//
+//给定二叉树 [3,9,20,null,null,15,7],
+//
+//3
+/// \
+//9  20
+///  \
+//15   7
+//返回它的最小深度  2.
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	} else if root.Left == nil && root.Right != nil {
+		return minDepth(root.Right) + 1
+	} else if root.Left != nil && root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	leftDepth := minDepth(root.Left)
+	rightDepth := minDepth(root.Right)
+	if leftDepth < rightDepth {
+		return leftDepth + 1
+	}
+
+	return rightDepth + 1
+}

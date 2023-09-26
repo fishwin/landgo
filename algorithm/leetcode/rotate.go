09c741cfc4c1252907a11e4f78ff08949d9fb5d1
zhanghaiyu <zhanghaiyu@p1.com>
2023-09-26 15:29:32 +0800

    feat: leetcode

--- algorithm/leetcode/rotate.go
+++ algorithm/leetcode/rotate.go
@@ -1,8 +1,8 @@
 package leetcode
 
-//
 //189. 轮转数组
-//给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+
+//给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
 //
 //
 //
@@ -36,114 +36,57 @@ package leetcode
 //你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
 
 func rotate(nums []int, k int) {
-	if len(nums) == 0 || k <= 0 {
-		return
-	}
-	if k > len(nums) {
-		k = k % len(nums)
-	}
-
-	ret := nums[len(nums)-k:]
-	ret = append(ret, nums[:len(nums)-k]...)
-	//nums = append(nums[len(nums)-k:], nums[:len(nums)-k]...)
-	copy(nums, ret)
-	// fmt.Println(nums)
-}
-
-// 给你一个整数数组 arr ，数组中的每个整数 互不相同 。另有一个由整数数组构成的数组 pieces，其中的整数也 互不相同 。
-// 请你以 任意顺序 连接 pieces 中的数组以形成 arr 。但是，不允许 对每个数组 pieces[i] 中的整数重新排序。
-//
-//如果可以连接 pieces 中的数组形成 arr ，返回 true ；否则，返回 false 。
-//
-//
-//示例 1：
-//
-//输入：arr = [15,88], pieces = [[88],[15]]
-//输出：true
-//解释：依次连接 [15] 和 [88]
-//示例 2：
-//
-//输入：arr = [49,18,16], pieces = [[16,18,49]]
-//输出：false
-//解释：即便数字相符，也不能重新排列 pieces[0]
-//示例 3：
-//
-//输入：arr = [91,4,64,78], pieces = [[78],[4,64],[91]]
-//输出：true
-//解释：依次连接 [91]、[4,64] 和 [78]
-
-func canFormArray(arr []int, pieces [][]int) bool {
-	if len(arr) == 0 {
-		return false
-	}
-
-	m := make(map[int]bool)
-	pArr, pPrev := 0, 0
-
-	for pArr < len(arr) {
-		x := 0
-		for ; x < len(pieces); x++ {
-			if m[x] {
-				continue
-			}
-			pPrev = pArr
-			for y := 0; y < len(pieces[x]); y++ {
-				if pieces[x][y] != arr[pArr] {
-					break
-				}
-				pArr++
-			}
-			if pArr-pPrev != len(pieces[x]) {
-				pArr = pPrev
-			} else {
-				m[x] = true
-				break
-			}
-		}
-		if x == len(pieces) {
-			return false
-		}
-	}
-
-	return true
-}
-
-//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
-//
-//请注意 ，必须在不复制数组的情况下原地对数组进行操作。
-//
-//
-//
-//示例 1:
-//
-//输入: nums = [0,1,0,3,12]
-//输出: [1,3,12,0,0]
-//示例 2:
-//
-//输入: nums = [0]
-//输出: [0]
-
-func moveZeroes2(nums []int) {
-	if len(nums) == 0 {
+	if len(nums) <= 0 {
 		return
 	}
 
-	c := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 0 {
-			c++
-		} else {
-			if c > 0 {
-				nums[i-c] = nums[i]
-			}
-		}
+	k %= len(nums)
+	ch := make(chan int, k)
+
+	for i := len(nums) - 1; i >= len(nums)-k; i-- {
+		ch <- nums[i]
 	}
 
-	for i := 1; i <= c; i++ {
-		nums[len(nums)-i] = 0
+	for i := len(nums) - 1 - k; i >= 0; i-- {
+		nums[i+k] = nums[i]
+	}
+
+	close(ch)
+	for v := range ch {
+		nums[k-1] = v
+		k--
 	}
 }
 
-func twoSum(numbers []int, target int) []int {
-	return nil
+func rotateV2(nums []int, k int) {
+	if len(nums) <= 0 {
+		return
+	}
+
+	k %= len(nums)
+
+	for k > 0 {
+		insertHead(nums[len(nums)-1], nums)
+		k--
+	}
+}
+
+func insertHead(target int, nums []int) {
+	for i := len(nums) - 2; i >= 0; i-- {
+		nums[i+1] = nums[i]
+	}
+	nums[0] = target
+}
+
+func rotateV1(nums []int, k int) {
+	if len(nums) <= 0 {
+		return
+	}
+
+	k %= len(nums)
+
+	var res []int
+	res = append(res, nums[len(nums)-k:]...)
+	res = append(res, nums[:len(nums)-k]...)
+	copy(nums, res)
 }
--- algorithm/leetcode/rotate_test.go
+++ algorithm/leetcode/rotate_test.go
@@ -1,28 +1,12 @@
 package leetcode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_rotate(t *testing.T) {
-	rotate([]int{1, 2}, 3)
-}
-
-func Test_canFormArray(t *testing.T) {
-	type args struct {
-		arr    []int
-		pieces [][]int
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := canFormArray(tt.args.arr, tt.args.pieces); got != tt.want {
-				t.Errorf("canFormArray() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	nums := []int{1, 2, 3, 4, 5}
+	rotate(nums, 2)
+	fmt.Println(nums)
 }
